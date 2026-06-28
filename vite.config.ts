0822f78194df699bf21beaa60790635d09f896d3
Lucas-Christian <lucas.christian.programmer@gmail.com>
2026-06-28 14:55:28 +0000

    feat: add svelte-sitemap integration for improved site mapping

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import tailwindcss from "@tailwindcss/vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import { svelteSitemap } from 'svelte-sitemap/vite';
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()],
+  plugins: [
+    tailwindcss(),
+    sveltekit(),
+    svelteSitemap({
+			domain: 'https://phoxia.org',
+		})
+  ],
   server: {
     port: 5173,
   },
